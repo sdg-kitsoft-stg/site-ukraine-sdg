e9d8bf06fa3ecd216592fc90aada08783ceea8c0
miroslavamarcuk <m.marchuk@kitsoft.ua>
2026-01-28 10:06:58 +0100

    SDG-18

--- _includes/assets/js/handleUI500page.js
+++
@@ -1,8 +0,0 @@
-function goTo500(error) {
-    if (!location.pathname.includes('/500')) {
-        location.href = '/500.html';
-    }
-}
-
-window.addEventListener('error', e => goTo500(e.error));
-window.addEventListener('unhandledrejection', e => goTo500(e.reason));

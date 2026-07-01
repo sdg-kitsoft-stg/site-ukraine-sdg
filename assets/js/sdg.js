70e6c9e555802e5f1e2b931aeb4d4ff88db1d071
VladStarynets <v.starynets@kitsoft.ua>
2026-07-01 08:29:09 +0000

    Deploying to gh-pages from @ sdg-kitsoft-stg/site-ukraine-sdg@78e22dd2b2305cbec69f920d0e8ff4525a23cab5 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4438,10 +4438,9 @@ function getMetadataCsvRows(selector) {
         var value = $(this).find('td').text().trim().replace(/\s+/g, ' ');
 
         if (key || value) {
-            rows.push([
-                formatCsvValue(key, false),
-                formatCsvValue(value, false)
-            ].join(';'));
+            const str = `${formatExcelCsvValue(key, false)}: ${formatExcelCsvValue(value, false)}`;
+
+            rows.push();
         }
     });
 
@@ -4515,9 +4514,9 @@ function toCsv(tableData, selectedSeries, selectedUnit) {
         lines.push('');
 
         if (lang === 'uk') {
-            lines.push(formatCsvValue('Поле метаданих Значення метаданих', false));
+            lines.push(formatCsvValue('Поле метаданих: Значення метаданих', false));
         } else {
-            lines.push(formatCsvValue('Metadata field Metadata value', false));
+            lines.push(formatCsvValue('Metadata field: Metadata value', false));
         }
 
         lines = lines.concat(metadataRows);
@@ -5115,10 +5114,9 @@ function getMetadataCsvRows(selector) {
         var value = $(this).find('td').text();
 
         if ($.trim(key) || $.trim(value)) {
-            rows.push([
-                formatExcelCsvValue(key),
-                formatExcelCsvValue(value)
-            ].join(';'));
+            const str = `${formatExcelCsvValue(key)}: ${formatExcelCsvValue(value)}`;
+
+            rows.push();
         }
     });
 
@@ -5182,9 +5180,9 @@ function downloadCsvWithMetadata(indicatorId) {
                 lines.push('');
 
                 if (lang === 'uk') {
-                    lines.push(formatCsvValue('Поле метаданих Значення метаданих', false));
+                    lines.push(formatCsvValue('Поле метаданих: Значення метаданих', false));
                 } else {
-                    lines.push(formatCsvValue('Metadata field Metadata value', false));
+                    lines.push(formatCsvValue('Metadata field: Metadata value', false));
                 }
 
                 lines = lines.concat(metadataRows);
